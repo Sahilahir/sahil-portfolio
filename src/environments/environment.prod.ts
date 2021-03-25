6ad867351d42f76f33382fe675cc76d289b708ee
Sahil Ahir <sahilahir333@gmail.com>
2021-03-25 12:09:46 +0530

    deployed with modal

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCQAzdfITgrN4mCfve1WShNE5WCEsxwylY",
+    authDomain: "sahil-portfolio-cbc6e.firebaseapp.com",
+    projectId: "sahil-portfolio-cbc6e",
+    storageBucket: "sahil-portfolio-cbc6e.appspot.com",
+    messagingSenderId: "178127940820",
+    appId: "1:178127940820:web:4c278ef4b335eac863df41",
+    measurementId: "G-B9B24Q8PYC"
+  }
 };
